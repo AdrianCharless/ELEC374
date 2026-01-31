2df7aa30031501ee910a5777ba9a5f9d19d9d859
AdrianCharless <126834852+AdrianCharless@users.noreply.github.com>
2026-01-30 21:43:06 -0500

    Update shiftRight.v

--- ELEC374_PHASE1/alu/shiftRight.v
+++ ELEC374_PHASE1/alu/shiftRight.v
@@ -1,9 +1,10 @@
 module shiftRight(
     input wire [31:0] A,
-    input wire [31:0] B,
+    // Only 5 bits because anything more would 31 would be the same as 31
+    input wire [4:0] B,
     output wire [31:0] result
     );
 
-
+assign result = A >> B;
 
 endmodule
