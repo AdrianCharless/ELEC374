ba224a09caf466513d5e07273da2fd7d7da52e19
douglaskchen <149521165+douglaskchen@users.noreply.github.com>
2026-02-02 16:45:39 -0500

    Update datapath.v

--- ELEC374_PHASE1/datapath.v
+++ ELEC374_PHASE1/datapath.v
@@ -1,1 +1,19 @@
-module Datapath( ... );
+module Datapath( 
+	input			clock, clear,
+	input [31:0]	A,
+	input [31:0]	RegAimm,
+	input			RZout, RAout, RBout,
+	input			RAin, RBin, RZin
+ );
+
+wire [31:0]		BusMuxOut, BusMuxInRZ, BusMuxInRA, BusMuxInRB;
+wire [31:0]		ZRegIn;
+
+register RA(
+	.clear(clear),
+	.clock(clock),
+	.enable(RAin),
+	.RegIn(A)
+	.output(A)
+);
+register RB();
