5b766f54af904120c0996b0f97a8a779240626da
RayGoldberg <raygoldberg@icloud.com>
2026-03-09 14:27:49 -0400

    Update hi_lo_unit.v

--- ELEC374_Phase2/hi_lo_unit.v
+++ ELEC374_Phase2/hi_lo_unit.v
@@ -1,20 +1,19 @@
 // hi_lo_unit.v
+// Drives the bus from HI or LO for mfhi / mflo
 
 module hi_lo_unit(
 
-    input [31:0] HI,
-    input [31:0] LO,
-
-    input HIout,
-    input LOout,
+    input  [31:0] HI,
+    input  [31:0] LO,
+    input         HIout,
+    input         LOout,
 
     output [31:0] Bus
 
 );
 
-assign Bus =
-        HIout ? HI :
-        LOout ? LO :
-        32'hz;
+assign Bus = HIout ? HI :
+             LOout ? LO :
+             32'hz;
 
 endmodule
