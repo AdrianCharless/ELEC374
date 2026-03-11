243037c894acbf296bad894627f65e257808bd58
Douglas Chen <douglaskchen@gmail.com>
2026-03-10 22:16:37 -0400

    added memory loading

--- memory/Memory.v
+++ memory/Memory.v
@@ -1,28 +1,34 @@
-// Memory Subsystem: MAR + MDR with MDMux + RAM
+// memory Subsystem: MAR + MDR with MDMux + RAM
 // - MAR captures address from BusMuxOut when MARin asserted
 // - RAM uses MAR[8:0] as address
 // - MDR captures either BusMuxOut (Read=0) or RAM data (Read=1) when MDRin asserted
 // - RAM read is asynchronous; RAM write is synchronous
 //
-// Exposes MDR_Q for the datapath bus as BusMuxInMDR.
+// exposes MDR_Q for the datapath bus as BusMuxInMDR.
 module Memory (
     input        clock,
     input        clear,
 
-    // Control
+    // control
     input        MARin,
     input        MDRin,
     input        Read,
     input        Write,
 
-    // Datapath bus connection
+    // datapath bus connection
     input  [31:0] BusMuxOut,
 
-    // Outputs back to datapath
+    // outputs back to datapath
     output [31:0] BusMuxInMDR,  // MDR contents -> bus input
     output [31:0] MAR_Q,        // optional debug
     output [31:0] RAM_rdata     // optional debug (this is Mdatain)
 );
+
+    // load memory
+    reg [31:0] mem [0:511];
+    initial begin
+        $readmemh("../memory_init.hex", mem);
+    end
 
     // MAR register (loads address from bus)
     Register MAR (clear, clock, MARin, BusMuxOut, MAR_Q);
