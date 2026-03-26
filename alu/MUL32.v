3f969d9768c743ab153e7e4c19424c7e2835a63b
Gia266 <154775233+Gia266@users.noreply.github.com>
2026-03-26 17:17:48 -0400

    Update MUL32.v

--- alu/MUL32.v
+++ alu/MUL32.v
@@ -46,42 +46,41 @@ module MUL32 (
     output wire [31:0] LO
 );
 
-    // 32 stages of carry-save accumulation w an array of 64 bit wires
+ 
     wire [63:0] sum_stage   [0:32];
     wire [63:0] carry_stage [0:32];
+    wire [63:0] carry_in    [0:32]; 
 
     assign sum_stage[0]   = 64'd0;
     assign carry_stage[0] = 64'd0;
+    assign carry_in[0]    = 64'd0;
 
     genvar i;
     generate
         for (i = 0; i < 32; i = i + 1) begin : MUL_CSA_CHAIN
             wire [63:0] pp_i;
 
-            // if B[i] = 1 → add A shifted left by i
-            // if B[i] = 0 → add 0
+            
             assign pp_i = B[i] ? ({32'd0, A} << i) : 64'd0;
 
             CarrySaveAdder64 csa64 (
                 .A   (sum_stage[i]),
-                .B   (carry_stage[i]),
+                .B   (carry_in[i]),
                 .C   (pp_i),
                 .S   (sum_stage[i+1]),
                 .Cout(carry_stage[i+1])
             );
+
+            assign carry_in[i+1] = carry_stage[i+1] << 1;
         end
     endgenerate
-
-    // Final collapse: product = sum + (carry << 1)
-    wire [63:0] carry_shifted;
-    assign carry_shifted = carry_stage[32] << 1;
 
     wire [63:0] product;
     wire        product_cout; // unused
 
     Add64 add64 (
         .A   (sum_stage[32]),
-        .B   (carry_shifted),
+        .B   (carry_in[32]),
         .Sum (product),
         .Cout(product_cout)
     );
