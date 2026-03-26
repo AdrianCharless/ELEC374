e7b1ed3536e7d48b6afa8112fd032adff6293863
Gia266 <154775233+Gia266@users.noreply.github.com>
2026-03-26 17:19:21 -0400

    Update R0_register.v

--- registers/R0_register.v
+++ registers/R0_register.v
@@ -6,17 +6,16 @@ module R0_register (
     input  [31:0] BusMuxOut,    
     output [31:0] BusMuxIn_R0   
 );
-
+ 
     reg [31:0] q;
-    initial q = 32'h0;
-
-    always @(posedge clock) begin
+ 
+    always @(posedge clock or posedge clear) begin
         if (clear)
             q <= 32'h0;
         else if (R0in)
             q <= BusMuxOut;
     end
-
+ 
     assign BusMuxIn_R0 = BAout ? 32'h0 : q;
-
+ 
 endmodule
